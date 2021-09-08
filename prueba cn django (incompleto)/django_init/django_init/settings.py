1d22ecf979f2f97202f42fbe060ee42785054aeb
Yane05 <69738680+Yane05@users.noreply.github.com>
2021-09-08 11:40:43 -0300

    Update settings.py

--- prueba cn django (incompleto)/django_init/django_init/settings.py
+++ prueba cn django (incompleto)/django_init/django_init/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rqa@9!ce#+p(mc+eg7!fl7oy&%a+c5h4&d26lxxxt*$aytf*qo'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
